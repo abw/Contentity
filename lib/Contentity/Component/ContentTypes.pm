af54d5481e0c591b53733715653ba2a31c5c7440
Andy Wardley <abw@wardley.org>
2014-03-24 07:42:36 +0000

    Worked Extensions component into ContentTypes

--- lib/Contentity/Component/ContentTypes.pm
+++ lib/Contentity/Component/ContentTypes.pm
@@ -3,7 +3,9 @@ package Contentity::Component::ContentTypes;
 use Contentity::Class
     debug     => 0,
     base      => 'Contentity::Component',
-    accessors => 'request_types response_types';
+    accessors => 'types accepts',
+    constants => 'TEXT_HTML',
+    utils     => 'split_to_list';
 
 
 sub init_component {
@@ -14,41 +16,72 @@ sub init_component {
 }
 
 sub init_content_types {
-    my ($self, $types) = @_;
-    my $reqt = $self->{ request_types  } = { };
-    my $rest = $self->{ response_types } = { };
+    my ($self, $config) = @_;
+    my $types  = $self->{ types   } = $config;
+    my $accept = $self->{ accepts } = { };
 
-    while (my ($key, $list) = each %$types) {
-        foreach my $item (@$list) {
-            $reqt->{ $item } = $key;
-            $rest->{ $key } ||= $item;
+    while (my ($key, $spec) = each %$types) {
+        my $ctype   = $spec->{ content_type };
+        my $acclist = $spec->{ accept };
+        my $urn     = $spec->{ urn } ||= $key;
+
+        if ($ctype) {
+            # add an accept mapping from the main content type to the entry
+            # e.g. application/json => { ... }
+            $accept->{ $ctype } = $spec;
+        }
+
+        if ($acclist) {
+            $acclist = split_to_list($acclist);
+
+            # also add accept mappings from any additional accept types
+            # e.g. text/json => { ... }
+            foreach my $item (@$acclist) {
+                $accept->{ $item } = $spec;
+            }
         }
     }
 
     if (DEBUG) {
-        $self->debug_data( request_types  => $self->request_types );
-        $self->debug_data( response_types => $self->response_types );
+        $self->debug_data( types   => $self->types   );
+        $self->debug_data( accepts => $self->accepts );
     }
 }
 
-sub request_type {
+sub type {
     my ($self, $type) = @_;
-    return $self->request_types->{ $type };
+    return $self->types->{ $type };
 }
 
-sub response_type {
-    my ($self, $type) = @_;
-    return $self->response_types->{ $type };
+sub content_type {
+    my ($self, $urn) = @_;
+    my $meta = $self->type($urn);
+    my $type = TEXT_HTML;
+    my $char;
+
+    if ($meta) {
+        $self->debug_data( "metadata for $urn content type" => $meta ) if DEBUG;
+        $type = $meta->{ content_type } || $type;
+        $char = $meta->{ charset };
+        $type .= "; charset=$char" if $char;
+    }
+
+    return $type;
+}
+
+sub file_content_type {
+    my ($self, $file) = @_;
+    return $self->content_type($file->extension);
 }
 
 sub accept_types {
     my ($self, $types) = @_;
-    my @keys = keys %$types;
-    my $reqt = $self->request_types;
+    my @keys    = keys %$types;
+    my $accepts = $self->accepts;
 
     foreach my $key (@keys) {
-        my $type = $reqt->{ $key } || next;
-        $types->{ $type } = $type;
+        my $type = $accepts->{ $key } || next;
+        $types->{ $type } = $type->{ urn };
     }
 
     $self->debug_data( merged_accept_types => $types ) if DEBUG;
@@ -56,7 +89,4 @@ sub accept_types {
     return $types;
 }
 
-
-
 1;
-
--- lib/Contentity/Component/Extensions.pm
+++ lib/Contentity/Component/Extensions.pm
@@ -1,5 +1,7 @@
 package Contentity::Component::Extensions;
 
+die __PACKAGE__, " is deprecated - moved in to Contentity::Component::ContentTypes";
+
 use Contentity::Class
     version => 0.01,
     debug   => 0,
@@ -17,6 +19,23 @@ sub init_component {
 sub extension {
     my ($self, $ext) = @_;
     return $self->config($ext);
+}
+
+sub content_type {
+    my ($self, $file) = @_;
+    my $ext  = $file->extension;
+    my $meta = $self->workspace->extension($ext);
+    my $type = TEXT_HTML;
+    my $char;
+
+    if ($meta) {
+        $self->debug_data( "metadata for $ext extension" => $meta ) if DEBUG;
+        $type = $meta->{ content_type } || $type;
+        $char = $meta->{ charset };
+        $type .= "; charset=$char" if $char;
+    }
+
+    return $type;
 }
 
 
