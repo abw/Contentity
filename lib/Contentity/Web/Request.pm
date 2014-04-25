19cea5de784e38f4e7e3ee5773440bb566965213
Andy Wardley <abw@wardley.org>
2014-04-25 15:47:09 +0100

    Added C::Web::Request and Response

---
+++ lib/Contentity/Web/Request.pm
@@ -0,0 +1,17 @@
+package Contentity::Web::Request;
+
+use Contentity::Web::Response;
+use Contentity::Class
+    version   => 0.02,
+    debug     => 0,
+    base      => 'Plack::Request Contentity::Base',
+    constant    => {
+        RESPONSE_MODULE => 'Contentity::Web::Response',
+    };
+
+
+sub new_response {
+    shift->RESPONSE_MODULE->new(@_);
+}
+
+1;
---
+++ lib/Contentity/Web/Response.pm
@@ -0,0 +1,9 @@
+package Contentity::Web::Response;
+
+use Contentity::Class
+    version   => 0.02,
+    debug     => 0,
+    base      => 'Plack::Response Contentity::Base';
+#    utils     => 'refaddr';
+
+1;
