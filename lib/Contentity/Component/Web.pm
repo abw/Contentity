f29e68d7e7225cebd575c0143f4d54b74d27e725
Andy Wardley <abw@wardley.org>
2015-06-30 15:29:30 +0100

    Cleaned up and documented C::Component::Web methods

--- lib/Contentity/Component/Web.pm
+++ lib/Contentity/Component/Web.pm
@@ -7,7 +7,7 @@ use Contentity::Class
     base      => 'Contentity::Component Contentity::Plack::Component',
     constants => 'BLANK :status :http_status :content_types',
     accessors => 'env context',
-    utils     => 'join_uri resolve_uri strip_hash extend split_to_list is_object',
+    utils     => 'extend join_uri resolve_uri strip_hash extend split_to_list split_to_hash is_object truelike bparams',
     codecs    => 'json',
     alias     => {
         _params => \&Contentity::Utils::params,
@@ -92,6 +92,15 @@ sub call_app {
 
 
 #-----------------------------------------------------------------------------
+# Config methods
+#-----------------------------------------------------------------------------
+
+sub config_flag {
+    my ($self, $name) = @_;
+    return truelike $self->config($name);
+}
+
+#-----------------------------------------------------------------------------
 # Request
 #-----------------------------------------------------------------------------
 
@@ -127,6 +136,11 @@ sub force_json {
     shift->context->accept_types->{ json } = 1;
 }
 
+
+#-----------------------------------------------------------------------------
+# URIs, paths, etc
+#-----------------------------------------------------------------------------
+
 sub uri {
     my $self = shift;
     my $base = $self->context->base;
@@ -143,98 +157,8 @@ sub script_name {
         : $base;
 }
 
-sub url {
-    my $self = shift;
-    my $url;
-
-    if (@_) {
-        my $name  = shift;
-        $self->debug("url lookup: $name") if DEBUG;
-
-        if ($url = $self->{ urls }->{ $name }) {
-            $url = $self->URL->new($url);
-            $self->debug("pre-defined URL mapping: $name => ", $url || "<NOT FOUND>") if DEBUG;
-        }
-        elsif ($name =~ m[^(\w+:)?/]) {
-            # if it's absolute (e.g. something like "/foo..." or "http://...")
-            # then we return it as it is
-            $url = $self->URL->new($name);
-            $self->debug("absolute URL: $name => $url\n") if DEBUG;
-        }
-        # TODO: elsif $site->url(...)     # site-wide URL
-        else {
-            # if it's not absolute then make it relative to the part of the
-            # current URL that the app has consumed
-            $url = $self->app_url($name);
-            $self->debug("app-relative URL: $name => $url\n") if DEBUG;
-        }
-        # add any parameters to URL or cache URLs without params for next time
-        $self->add_url_params($url, @_)
-            if @_;
-    }
-    else {
-        $url = $self->app_url;
-    }
-    $self->debug("URL => $url") if DEBUG;
-    return $url;
-}
-
-sub app_url {
-    my $self = shift;
-    my $done = $self->path->path_done;
-    $done = resolve_uri($done, shift) if @_;
-    $self->debug("app_url path done: $done") if DEBUG;
-    my $url  = $self->URL->new($done);
-    $self->add_url_params($url, @_) if @_;
-    $self->debug("app_url: $url") if DEBUG;
-    return $url;
-}
-
-sub add_url_params {
-    my $self = shift;
-    my $url  = shift;
-    if (@_) {
-        my $params = Badger::Utils::params(@_);
-        strip_hash($params);
-        $self->debug_data( adding => $params ) if DEBUG;
-        $url->params($params);
-    }
-    return $url;
-}
-
-sub full_url {
-    my $self = shift;
-
-    return $self->app_url(
-        $self->path->todo
-    );
-}
-
-sub url_with_params {
-    my $self = shift;
-    my $url  = $self->app_url;
-    $self->add_url_params($url, $self->params);
-    return $url;
-}
-
-
-#-----------------------------------------------------------------------
-# Path
-#-----------------------------------------------------------------------
-
 sub path {
     shift->context->path;
-}
-
-sub XXclear_path_done {
-    my $done = shift->path_done;
-    @$done = ();
-}
-
-sub XXshift_path_todo {
-    my $self = shift;
-    my $todo = $self->path->todo;
-    return shift @$todo;
 }
 
 
@@ -317,58 +241,85 @@ sub user {
     shift->context->user;
 }
 
-
 #-----------------------------------------------------------------------------
-# authorisation roles
+# URL generation
 #-----------------------------------------------------------------------------
 
-sub roles_for_user {
+sub url {
     my $self = shift;
-    return  $self->{ roles_for_user }
-        ||= $self->roles_hash('roles.user');
-}
+    my $url;
 
-sub roles_for_guest {
-    my $self = shift;
-    return  $self->{ roles_for_guest }
-        ||= $self->roles_hash('roles.guest');
-}
+    if (@_) {
+        my $name  = shift;
+        $self->debug("url lookup: $name") if DEBUG;
 
-sub roles_hash {
-    my ($self, $key) = @_;
-    return {
-        map { $_ => 1 }
-        @{ split_to_list( $self->workspace->config($key) ) }
-    };
-}
-
-sub realm_roles {
-    my $self  = shift;
-    my $login = $self->login;
-    my $roles = { };
-
-    if ($login) {
-        # merge in the roles explicitly granted to them as realm roles,
-        # and any global roles for users defined in config/roles.yaml
-        extend(
-            $roles,
-            $login->realm_roles_hash(
-                $self->{ realm } ||= $self->workspace->realm,
-            ),
-            $self->roles_for_user
-        );
-        $self->debug_data( merged_roles => $roles ) if DEBUG;
+        if ($url = $self->{ urls }->{ $name }) {
+            $url = $self->URL->new($url);
+            $self->debug("pre-defined URL mapping: $name => ", $url || "<NOT FOUND>") if DEBUG;
+        }
+        elsif ($name =~ m[^(\w+:)?/]) {
+            # if it's absolute (e.g. something like "/foo..." or "http://...")
+            # then we return it as it is
+            $url = $self->URL->new($name);
+            $self->debug("absolute URL: $name => $url\n") if DEBUG;
+        }
+        # TODO: elsif $site->url(...)     # site-wide URL
+        else {
+            # if it's not absolute then make it relative to the part of the
+            # current URL that the app has consumed
+            $url = $self->app_url($name);
+            $self->debug("app-relative URL: $name => $url\n") if DEBUG;
+        }
+        # add any parameters to URL or cache URLs without params for next time
+        $self->add_url_params($url, @_)
+            if @_;
     }
     else {
-        $self->debug("no login") if DEBUG;
-        extend(
-            $roles,
-            $self->roles_for_guest,
-        );
+        $url = $self->app_url;
     }
-
-    return $roles;
+    $self->debug("URL => $url") if DEBUG;
+    return $url;
 }
+
+sub app_url {
+    my $self = shift;
+    my $done = $self->path->done;
+    $done = resolve_uri($done, shift) if @_;
+    $self->debug("app_url path done: $done") if DEBUG;
+    my $url  = $self->URL->new($done);
+    $self->add_url_params($url, @_) if @_;
+    $self->debug("app_url: $url") if DEBUG;
+    return $url;
+}
+
+sub add_url_params {
+    my $self = shift;
+    my $url  = shift;
+    if (@_) {
+        my $params = Badger::Utils::params(@_);
+        strip_hash($params);
+        $self->debug_data( adding => $params ) if DEBUG;
+        $url->params($params);
+    }
+    return $url;
+}
+
+sub full_url {
+    my $self = shift;
+
+    return $self->app_url(
+        $self->path->todo
+    );
+}
+
+# TODO: delete me
+sub OLD_url_with_params {
+    my $self = shift;
+    my $url  = $self->app_url;
+    $self->add_url_params($url, $self->params);
+    return $url;
+}
+
 
 
 
@@ -586,7 +537,49 @@ sub send_error_msg {
 }
 
 #-----------------------------------------------------------------------------
-# Methods for redirecting user to login to access a page
+# Pending data on redirect - sometimes we want to redirect a user to a
+# different page and flash a message to them when they get there.  We store
+# pending data in the session and provide various methods of convenience to
+# add pending data (typically status messages) before redirecting.
+#-----------------------------------------------------------------------------
+
+sub save_pending_data {
+    my $self    = shift;
+    my $session = $self->session;
+    my $data    = $session->data;
+    my $pending = $data->{ pending_data } ||= { };
+
+    extend($pending, @_);
+    $session->save;
+
+    $self->debug_data("added pending_data to session: ", $pending) if DEBUG;
+}
+
+sub load_pending_data {
+    my $self    = shift;
+    my $data    = $self->session->data;
+    my $pending = $data->{ pending_data };
+    $self->set($pending) if $pending;
+    return $pending;
+}
+
+sub take_pending_data {
+    my $self    = shift;
+    my $session = $self->session;
+    my $data    = $session->data;
+    my $pending = delete $data->{ pending_data };
+    if ($pending) {
+        $self->set($pending);
+        $session->save;
+    }
+    return $pending;
+}
+
+
+
+
+#-----------------------------------------------------------------------------
+# Methods for redirecting user
 #-----------------------------------------------------------------------------
 
 sub redirect {
@@ -596,6 +589,67 @@ sub redirect {
     );
 }
 
+sub redirect_pending_data {
+    my $self = shift;
+    my $url  = shift;
+    my $data = bparams(@_);
+
+    $self->save_pending_data($params);
+    return $self->redirect($url);
+}
+
+sub redirect_status {
+    my $self = shift;
+    my $url  = shift;
+    my $type = shift;
+    my $text = join(' ', @_);
+
+    $self->save_pending_data(
+        $type => $text
+    );
+
+    if ($self->accept_json) {
+        return $self->send_json(
+            extend(
+                { },
+                $self->json_data,
+                {
+                    redirect => $url,
+                    status   => $type,
+                    $type    => $text,
+                }
+            )
+        );
+    }
+    return $self->redirect($url);
+}
+
+sub redirect_status_msg {
+    my $self = shift;
+    my $url  = shift;
+    my $type = shift;
+    my $text = $self->message(@_);
+    return $self->redirect_status($url, $type, $text);
+}
+
+sub redirect_success {
+    return shift->redirect_status(shift, success => @_);
+}
+
+sub redirect_error {
+    return shift->redirect_status(shift, error => @_);
+}
+
+sub redirect_success_msg {
+    return shift->redirect_status_msg(shift, success => @_);
+}
+
+sub redirect_error_msg {
+    return shift->redirect_status_msg(shift, error => @_);
+}
+
+
+
 sub redirect_login_url {
     my $self   = shift;
     my $login  = shift;
@@ -604,6 +658,7 @@ sub redirect_login_url {
     my $msg    = @_ ? join('', @_) : $self->message('redirect_login');
 
     $self->debugf("user was going to: $final") if DEBUG or 1;
+    $self->debug("message: $msg") if DEBUG or 1;
 
     # ensure URL is stringified
     $final = "$final";
@@ -672,7 +727,230 @@ sub model {
 
 __END__
 
-=head1 METHODS
+=head1 FRAMEWORK INTEGRATION METHODS
+
+=head2 call(\%env)
+
+External entry point for web components running under the Plack web
+application framework.
+
+=head run()
+
+Internal internal entry point for web components.
+Subclasses are expected to re-implement this method.  In this base
+class it throws an error indicating that it is not implemented.
+
+=head2 CONTEXT METHODS
+
+=head2 new_context( env => \%env )
+
+Creates a new context object to handle a web application request and
+response.  Should be passed a named parameter that provides a reference
+to the Plack environment.
+
+=head2 call_in_context($context)
+
+Runs this web app in the context of the object passed as an argument.
+
+=head2 call_app($app)
+
+Dispatches another web application or component in the same content as
+the current component.
+
+=head1 CONFIGURATION METHODS
+
+=head2 config_flag($name)
+
+Looks for the configuration item C<$name> and tests if it is truelike.
+
+=head1 REQUEST METHODS
+
+=head2 request()
+
+Returns the C<Plack::Request> object
+
+=head2 method()
+
+Returns the HTTP request method, e.g. C<GET>, C<POST>, etc.
+
+=head2 GET
+
+Returns true if the request L<method()> is C<GET>.
+
+=head2 POST
+
+Returns true if the request L<method()> is C<POST>.
+
+=head2 DELETE
+
+Returns true if the request L<method()> is C<DELETE>.
+
+=head2 accept($type)
+
+Delegates to the context C<accept_type()> method to examine the
+C<http-accept> header.  If an argument is passed then it becomes
+a boolean test to match the specified type.
+
+=head2 accept_json()
+
+Returns a boolean value to indicate if the C<http-accept> header
+indicates the client is expecting a JSON response.
+
+=head2 forces_json()
+
+Forces the L<accept_json()> method to always return true for the
+lifetime of this request/response context.
+
+=head1 REQUEST PATH METHODS
+
+=head2 uri($path)
+
+Returns the base URI for the current web component.  Typically this
+will be the "mount point" specified in a C<locations.yaml> file or
+hard-coded in an Apache configuration file.  If an additional URI path
+is passed as an argument then it will be appended to the base path.
+
+=head2 script_name($path)
+
+Delegates to the method of the same name in the request to return the
+full path to the script.  If a path is provided as an argument then it
+will be appended to the base script path.
+
+=head2 path()
+
+Delegates to the context C<path()> method to return the path object.
+
+=head2 REQUEST PARAMETER METHODS
+
+=head2 params()
+
+Returns a hashref of request parameters.
+
+=head2 param($name)
+
+Returns a single request parameter.
+
+=head2 param_list($names)
+
+Delegates to the C<Plack::Request> C<get_all()> method.
+
+=head2 some_params($spec, $params)
+
+Extracts a hash reference of parameters that are defined and have a
+non-zero length.  The C<$spec> should be a reference to an array of
+names or a whitespace delimited string of parameter names.  If C<$params>
+isn't specified then it calls C<params()> to fetch the current params.
+
+    my $subset = $self->some_params('username password');
+
+=head1 CONTEXT DATA METHODS
+
+Most of these methods simply delegate to the context object.
+
+=head2 get($name)
+
+Get the value of a context variable.
+
+=head2 set($data)
+
+Set one of more items of context data.
+
+=head2 data($data)
+
+Get or set the context data.
+
+=head2 get_cookie($name)
+
+=head2 set_cookie($name, $value)
+
+=head2 session()
+
+=head2 login()
+
+=head2 user()
+
+=head1 RESPONSE METHODS
+
+=head2 response()
+
+=head2 redirect_response($body)
+
+=head2 not_found_response($body)
+
+=head2 forbidden_response($body)
+
+=head2 error_response($body)
+
+=head2 type_content_response($type, $content)
+
+=head2 send_text($text)
+
+=head2 send_html($html)
+
+=head2 send_xml($xml)
+
+=head2 send_json($data)
+
+If a C<callback> parameter is specified then it delegates to
+L<send_jsonp()>.  Otherwise it sends a normal JSON response.
+
+=head2 send_jsonp($data)
+
+=head2 send_json_success($text)
+
+=head2 send_json_error($error)
+
+=head2 send_json_error_msg($format, @args)
+
+Calls the C<message()> method to expand a pre-defined message format
+with the additional arguments passed.  Then it passes the error message
+generated to L<send_json_error()>.
+
+=head2 json_error_message($error)
+
+Called by L<send_json_error()> to ensure that the C<$error> is
+plain text.  If C<$error> is an exception object (C<Badger::Exception>)
+then is calls the C<$info> method on it to return a textual error message.
+
+=head2 send_redirect($url)
+
+Delegates to L<redirect_response()>.
+
+=head2 send_not_found($body)
+
+Delegates to L<not_found_response()>
+
+=head2 send_not_found_msg($format, @args)
+
+Wrapper around L<send_not_found()> using L<message()> to expand the
+message format.
+
+=head2 send_forbidden($error)
+
+Calls L<send_json_error($error)> if L<wants_json()> otherwise calls
+L<send_forbidden_html($error)>.
+
+=head2 send_forbidden_html($error)
+
+Calls C<error_response()>.  Subclasses can redefine this method to
+embed the error in an HTML page.
+
+=head2 send_error($error)
+
+Calls L<send_json_error($error)> if L<wants_json()> otherwise calls
+L<send_error_html($error)>.
+
+=head2 send_error_msg($format, @args)
+
+Wrapper around L<send_error()> using L<message()> to expand the
+message format.
+
+=head2 sub send_error_html($error)
+
+Calls C<error_response()>.  Subclasses can redefine this method to
+embed the error in an HTML page.
+
+=head1 REDIRECT METHODS
 
 =head2 redirect_login_url($login_url, $final_url, $final_params, $msg)
 
@@ -712,3 +990,70 @@ Defaults to C</auth/login>.
     # in app/XXX.yaml
     urls:
       login: /path/to/login.html
+
+=head2 pending_redirect_login_url()
+
+If the session has a C<redirect_login> data item then this method
+returns a redirect to that URL.  Any C<redirect_params> will also be
+added the URL.  The C<redirect_login> and C<redirect_params> are
+removed from the session data.
+
+=head1 PENDING DATA METHODS
+
+Sometimes we want to redirect a user to a different page and flash a
+message to them when they get there.  We store pending data in the
+session and provide various methods of convenience to add pending data
+(typically status messages) before redirecting.
+
+=head2 save_pending_data($data)
+
+Saved pending data.
+
+=head2 load_pending_data()
+
+Loads pending data from the session does not delete it.
+
+=head2 take_pending_data()
+
+Loads and deletes pending data from the session.
+
+=head1 URL GENERATION METHODS
+
+=head2 url($path)
+
+Generates a URL.  Named URLs can be defined in the app configuration
+(e.g. 'login => /path/to/login').  Otherwise the L<app_url()> is used
+as the base.  Any additional path passed as an argument is appended to
+the URL.
+
+=head2 app_url($path, $params)
+
+Generates a URL using the application path plus any sections consumed
+from the path (C<$self->path->done>) as the base.    The C<$path>
+url is then appended along with any parameters passed by hash reference.
+
+=head2 add_url_params($url, $params)
+
+Adds parameters to the end of a URL object.
+
+=head2 full_url()
+
+Returns the full URL of the request, constructed from the L<app_url()>
+plus the remaining path (C<$self->path->todo>).
+
+=head1 AUTHOR
+
+Andy Wardley L<http://wardley.org/>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2006-2015 Andy Wardley.  All Rights Reserved.
+
+This module is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+L<Contentity::Component>, L<Contentity::Web::App>
+
+=cut
