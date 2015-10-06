d459b20471e8e16a7f81947dc23a8156eb915d23
Andy Wardley <abw@wardley.org>
2015-10-06 16:23:54 +0100

    Minor fix to sass builder

--- lib/Contentity/Configure/Sass.pm
+++ lib/Contentity/Configure/Sass.pm
@@ -6,27 +6,26 @@ use Contentity::Class
     base    => 'Contentity::Configure::App';
 
 sub run {
-    my $self   = shift;
-    my $config = $self->config;
+    my $self    = shift;
+    my $config  = $self->config;
+    my $site    = $self->workspace;
+    my $project = $site->project;
+
+    $self->debug("RUN") if DEBUG;
 
     $self->prompt_expr([
         [ selected => "Pre-building SASS:\n" ],
     ]);
 
-    # do the pre-sass build
-    my $options = {
-        renderer => 'sassprep'
-    };
-    my $spbuilder = $site->builder($options);
-    $self->debug("got sassprep builder: $spbuilder") if DEBUG;
-    $spbuilder->build;
-
     $self->prompt_expr([
         [ selected => "Building SASS:\n" ],
     ]);
 
+    $self->debug("site:$site") if DEBUG;
+    $self->debug("project:$project") if DEBUG;
 
-    $project->sass_builder($config)->build;
+    #$site->sass_builder($config)->build;
+    $site->component('builder.sass')->build;
 }
 
 
