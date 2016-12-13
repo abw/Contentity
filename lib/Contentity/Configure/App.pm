2aaf43b505f90442a1293a60efd945b0a68d8510
Andy Wardley <abw@wardley.org>
2016-12-13 10:50:54 +0000

    Removed old colourise() and prompt_colour() methods from prompter and configure app

--- lib/Contentity/Configure/App.pm
+++ lib/Contentity/Configure/App.pm
@@ -359,9 +359,8 @@ class->methods(
         }
     }
     qw(
-        prompt prompt_list prompt_expr prompt_newline prompt_error prompt_colour
+        prompt prompt_list prompt_expr prompt_newline prompt_error
         prompt_title prompt_about prompt_action prompt_comment prompt_entry
-        colourise
     )
 );
 
